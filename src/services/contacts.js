078eebcdf3e335936889705941020eaf59c25cc1
sauergru <sauergru@gmail.com>
2024-08-14 19:44:45 +0200

    1 + 2 step

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,6 +6,6 @@ export const getAllContacts = async () => {
 };
 
 export const getContactById = async (contactId) => {
-  const student = await ContactsCollection.findById(contactId);
-  return student;
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
 };
